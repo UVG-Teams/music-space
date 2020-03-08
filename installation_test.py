3a7a342fe7777ded501f6438ed75afd304cce1dd
FR98 <ros18676@uvg.edu.gt>
2020-03-08 16:00:31 -0600

    Initial

---
+++ installation_test.py
@@ -0,0 +1,15 @@
+import django
+import psycopg2
+
+print(django.get_version())
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'proyecto1dbuvg',
+#         'USER': 'postgres',
+#         'PASSWORD': 'admin',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
