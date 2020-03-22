73b8fe186794ee540509babce1db783c3b875561
FR98 <ros18676@uvg.edu.gt>
2020-03-22 15:23:19 -0600

    Login y Logout

--- music_site/music_streaming/urls.py
+++ music_site/music_streaming/urls.py
@@ -1,10 +1,23 @@
 from django.urls import path, include
+from django.contrib.auth import views as auth_views
 
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
 
+    path('accounts/', include('django.contrib.auth.urls')),
+
+    path('accounts/login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+
+    path('home/', views.home, name='home'),
+
+    path('musicStreaming/admin/', views.admin, name='admin'),
+
+    path('reports/', views.reports, name='reports'),
+
+    # path('logout/', views.logout_view, name='logout'),
+    
     path('albums/', include('albums.urls')),
 
     path('artists/', include('artists.urls')),
@@ -15,7 +28,4 @@ urlpatterns = [
 
     path('tracks/', include('tracks.urls')),
 
-    path('reports/', views.reports, name='reports'),
-
-    path('musicStreaming/admin/', views.admin, name='admin'),
 ]
