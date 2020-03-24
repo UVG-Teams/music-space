e53c69fa29a06ed75ee090b4995fc1852b0fb2fa
FR98 <ros18676@uvg.edu.gt>
2020-03-23 19:23:29 -0600

    Estructura

--- music_site/artists/urls.py
+++ music_site/artists/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = 'artists'
 urlpatterns = [
-    path('', views.index, name='artistsIndex'),
+    path('', views.index, name='index'),
 
     path('<int:id>/', views.detail, name='detail'),
 ]
--- music_site/genres/urls.py
+++ music_site/genres/urls.py
@@ -2,8 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = 'genres'
 urlpatterns = [
-    path('', views.index, name='genresIndex'),
+    path('', views.index, name='index'),
 
     path('<int:id>/', views.detail, name='detail'),
 ]
