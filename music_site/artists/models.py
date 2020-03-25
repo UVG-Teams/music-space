d6cbab4ecfd0ee4cdd159b15903aa1151fbf0d2b
FR98 <ros18676@uvg.edu.gt>
2020-03-25 11:25:59 -0600

    Permisos

--- music_site/artists/models.py
+++ music_site/artists/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Artist
 class Artist(models.Model):
-    artistid = models.AutoField(primary_key=True, blank=False, null=False, )
+    artistid = models.IntegerField(primary_key=True, blank=False, null=False)
     name = models.CharField(max_length=100, blank=False, null=False)
 
     class Meta:
